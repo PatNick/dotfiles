3491626073d0a83377de7d4f3d6fbe4fbc4d047c
Patrick Nickle <patrick.nickle@gmail.com>
2022-03-21 15:01:08 -0400

    Color tweaking after nightfox update

--- nvim/.config/nvim/lua/patnick/colors.lua
+++ nvim/.config/nvim/lua/patnick/colors.lua
@@ -3,31 +3,34 @@ local nightfox = require('nightfox')
 -- This function set the configuration of nightfox. If a value is not passed in the setup function
 -- it will be taken from the default configuration above
 nightfox.setup({
-  fox = "nightfox",
-  transparent = true,
-  alt_nc = false,
-  styles = {
-    comments = "italic", -- change style of comments to be italic
-    keywords = "bold", -- change style of keywords to be bold
-    functions = "italic,bold" -- styles can be a comma separated list
-  },
-  inverse = {
-    match_paren = true, -- inverse the highlighting of match_parens
-  },
-  hlgroups = {
-    TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
-    LspCodeLens = { bg = "#000000", style = "italic" },
-    TelescopeBorder = { fg = "${fg_popup}", bg = "${bg_popup}" },
-    TelescopePromptBorder = { fg = "${fg_visual}", bg = "${bg_visual}" },
-    TelescopePromptNormal = { fg = "${fg_sidebar}", bg = "${bg_visual}" },
-    TelescopePromptPrefix = { fg = "${fg_sidebar}", bg = "${bg_visual}" },
-    TelescopeNormal = { fg = "${fg_sidebar}", bg = "${bg_popup}" },
-    TelescopePreviewTitle = { fg = "${bg_popup}", bg = "${blue}" },
-    TelescopePromptTitle = { fg = "${bg_popup}", bg = "${blue}" },
-    TelescopeResultsTitle = { fg = "${bg_popup}", bg = "${blue}" },
-    TelescopeMatching = { fg = "${error}", bg = "${NONE}" },
-  }
+    options = {
+        transparent = true,
+        dim_inactive = false,
+        styles = {
+            comments = "italic", -- change style of comments to be italic
+            keywords = "bold", -- change style of keywords to be bold
+            functions = "italic,bold" -- styles can be a comma separated list
+        },
+        inverse = {
+            match_paren = false, -- inverse the highlighting of match_parens
+            visual = false,
+            search = true,
+        },
+        hlgroups = {
+            TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
+            LspCodeLens = { bg = "#000000", style = "italic" },
+            TelescopeBorder = { fg = "${fg_popup}", bg = "${bg_popup}" },
+            TelescopePromptBorder = { fg = "${fg_visual}", bg = "${bg_visual}" },
+            TelescopePromptNormal = { fg = "${fg_sidebar}", bg = "${bg_visual}" },
+            TelescopePromptPrefix = { fg = "${fg_sidebar}", bg = "${bg_visual}" },
+            TelescopeNormal = { fg = "${fg_sidebar}", bg = "${bg_popup}" },
+            TelescopePreviewTitle = { fg = "${bg_popup}", bg = "${blue}" },
+            TelescopePromptTitle = { fg = "${bg_popup}", bg = "${blue}" },
+            TelescopeResultsTitle = { fg = "${bg_popup}", bg = "${blue}" },
+            TelescopeMatching = { fg = "${error}", bg = "${NONE}" },
+        },
+    },
 })
 
 -- Load the configuration set above and apply the colorscheme
-nightfox.load()
+vim.cmd("colorscheme nightfox")
