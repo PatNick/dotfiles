e7ccc1aece11274b2b0518db23f90f1a473a98c5
Patrick Nickle <patrick.nickle@gmail.com>
2022-03-22 15:15:22 -0400

    Useless comments...

--- nvim/.config/nvim/lua/patnick/colors.lua
+++ nvim/.config/nvim/lua/patnick/colors.lua
@@ -7,9 +7,9 @@ nightfox.setup({
         transparent = false,
         dim_inactive = true,
         styles = {
-            comments = "italic", -- change style of comments to be italic
-            keywords = "bold", -- change style of keywords to be bold
-            functions = "italic,bold" -- styles can be a comma separated list
+            comments = "italic",
+            keywords = "bold",
+            functions = "italic,bold"
         },
         inverse = {
             match_paren = false,
