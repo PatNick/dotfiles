ddf82f18c152ee31285207110e52b1ddb9403da8
Patrick Nickle <patrick.nickle@gmail.com>
2022-04-04 10:16:22 -0400

    Solid color bg

--- awesome/.config/awesome/deco/wallpaper.lua
+++ awesome/.config/awesome/deco/wallpaper.lua
@@ -3,14 +3,16 @@ local beautiful = require("beautiful")
 
 function set_wallpaper(s)
     -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
+    -- if beautiful.wallpaper then
+        -- local wallpaper = beautiful.wallpaper
+        -- -- If wallpaper is a function, call it with the screen
+        -- if type(wallpaper) == "function" then
+            -- wallpaper = wallpaper(s)
+        -- end
+        -- gears.wallpaper.maximized(wallpaper, s, true)
+    -- end
+
+    gears.wallpaper.set("#192330")
 end
 
 screen.connect_signal("property::geometry", set_wallpaper)
