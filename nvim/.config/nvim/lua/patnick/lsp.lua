a2d06484376dfc104c8d74a54ff6be99d991a365
Patrick Nickle <patrick.nickle@gmail.com>
2022-08-30 10:21:21 -0400

    Tweaking more stuff, removing crud I don't use

--- nvim/.config/nvim/lua/patnick/lsp.lua
+++ nvim/.config/nvim/lua/patnick/lsp.lua
@@ -9,7 +9,7 @@ local my_attach = function(client, buffer)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
     vim.keymap.set('n', '<leader>vd', vim.lsp.buf.definition, {buffer=0})
     vim.keymap.set('n', '<leader>vi', vim.lsp.buf.implementation, {buffer=0})
-    vim.keymap.set('n', '<leader>vsh', vim.lsp.buf.signature_help, {buffer=0})
+    vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, {buffer=0})
     vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, {buffer=0})
     vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, {buffer=0})
     vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, {buffer=0})
@@ -19,7 +19,7 @@ local my_attach = function(client, buffer)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {buffer=0})
 end
 
-local servers = { 'tsserver', 'clangd', 'pyright', 'gopls', 'vimls', 'bashls', 'ansiblels' }
+local servers = { 'tsserver', 'clangd', 'pyright', 'gopls', 'bashls', 'ansiblels' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         on_attach = my_attach,
