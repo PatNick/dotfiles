0cc8551120b51209730140ed357cbd93e5ba42fd
Patrick Nickle <patrick.nickle@gmail.com>
2022-10-24 13:00:59 -0400

    I'm an idiot

--- nvim/.config/nvim/after/plugin/keymap/remap.lua
+++ nvim/.config/nvim/after/plugin/keymap/remap.lua
@@ -6,8 +6,8 @@ nnoremap("<leader>y", "\"+y")
 vnoremap("<leader>y", "\"+y")
 nnoremap("Y", "yg$")
 
-nnoremap("<C-j", "<cmd>cnext<cr>zz")
-nnoremap("<C-k", "<cmd>cprev<cr>zz")
+nnoremap("<C-j>", "<cmd>cnext<cr>zz")
+nnoremap("<C-k>", "<cmd>cprev<cr>zz")
 
 nnoremap("<leader>pv", "<cmd>Ex<cr>")
 nnoremap("<leader>x", "<cmd>!chmod +x %<cr", { silent = true })
