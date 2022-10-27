d0018bd7c8555f0dfe3d7ec2523eac59a8c4cf50
Patrick Nickle <patrick.nickle@gmail.com>
2022-10-27 11:27:00 -0400

    Reorganize telescope

--- nvim/.config/nvim/lua/nuttywombat/telescope.lua
+++ nvim/.config/nvim/lua/nuttywombat/telescope.lua
@@ -1,10 +1,43 @@
+local previewers = require("telescope.previewers")
+local sorters = require("telescope.sorters")
+local actions = require("telescope.actions")
+
+require('telescope').setup{
+
+    defaults = {
+        file_sorter = sorters.get_fzf_sorter,
+        prompt_prefix = " >",
+        color_devicons = true,
+
+        file_previewer = previewers.vim_buffer_cat.new,
+        grep_previewer = previewers.vim_buffer_vimgrep.new,
+        qflist_previewer = previewers.vim_buffer_qflist.new,
+
+        mappings = {
+            i = {
+                ["<C-x>"] = false,
+                ["<C-q>"] = actions.send_to_qflist,
+            },
+        },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case"
+        }
+    },
+}
+
+require('telescope').load_extension('fzf')
+
 local M = {}
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
         prompt_title = "< VimRC >",
         hidden = true,
-        cwd = "~/dotfiles/nvim_lua/.config/nvim",
+        cwd = "~/dotfiles/nvim/.config/nvim",
     })
 end
-
 return M
