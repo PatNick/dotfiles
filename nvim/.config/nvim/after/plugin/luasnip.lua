d939a6cac549a2b236b85d55a70ca8859f650e75
Patrick Nickle <patrick.nickle@gmail.com>
2022-11-25 07:18:04 -0500

    Setup snippets

---
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -0,0 +1,7 @@
+local luasnip = require('luasnip')
+local types = require('luasnip.util.types')
+
+luasnip.config.set_config {
+    history = true,
+    updateevents = "Textchanged,TextChangedI",
+}
