660d5484f6d936c3aee9772b3987fb56676eb406
Patrick Nickle <patrick.nickle@gmail.com>
2022-11-25 07:18:15 -0500

    Quick access to grep cht.sh

--- bin/.local/bin/cht.sh
+++ bin/.local/bin/cht.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 languages=$(echo "golang c cpp java python typescript" | tr " " "\n")
-core_utils=$(echo "find xargs sed awk" | tr " " "\n")
+core_utils=$(echo "find xargs sed grep awk" | tr " " "\n")
 selected=$(echo -e "$languages\n$core_utils" | fzf)
 
 read -p "query: " query
