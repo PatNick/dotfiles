4bfec83358a70d2bd59083660cd9620b30bafa04
Patrick Nickle <patrick.nickle@gmail.com>
2022-12-01 11:46:33 -0500

    Refining the cht.sh script

--- bin/.local/bin/cht.sh
+++ bin/.local/bin/cht.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 languages=$(echo "golang c cpp java python typescript" | tr " " "\n")
-core_utils=$(echo "find xargs sed grep awk" | tr " " "\n")
+core_utils=$(echo "git find xargs sed grep awk" | tr " " "\n")
 selected=$(echo -e "$languages\n$core_utils" | fzf)
 
 read -p "query: " query
 
 if echo "$languages" | grep -qs "$selected"; then
-    bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less "
+    tmux splitw -h -t ! bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less "
 else
-    bash -c "curl cht.sh/$selected~$query | less"
+    tmux splitw -h -t ! bash -c "curl cht.sh/$selected~$query | less"
 fi
