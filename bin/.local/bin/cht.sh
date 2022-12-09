429d55916dc9b8b5a6bc0c8fff4e2a73708b554e
Patrick Nickle <patrick.nickle@gmail.com>
2022-12-09 12:09:08 -0500

    Refactoring cht.sh script to bet better

--- bin/.local/bin/cht.sh
+++ bin/.local/bin/cht.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-languages=$(echo "golang c cpp java python typescript rust" | tr " " "\n")
-core_utils=$(echo "find xargs sed grep awk git docker" | tr " " "\n")
-selected=$(echo -e "$languages\n$core_utils" | fzf)
+selected=$(cat ~/.tmux-cht-languages ~/.tmux-cht.utils | fzf)
 
 read -p "query: " query
 
-if echo "$languages" | grep -qs "$selected"; then
+if grep -qs "$selected" ~/.tmux-cht-languages; then
     tmux splitw -h -t ! bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less "
 else
     tmux splitw -h -t ! bash -c "curl cht.sh/$selected~$query | less"
