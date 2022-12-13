3ff308be1ba8c13b95320a8eab99969d3de65e0c
Patrick Nickle <patrick.nickle@gmail.com>
2022-12-12 20:08:11 -0500

    Quotes

--- nvim/.config/nvim/after/plugin/keymap/telescope.lua
+++ nvim/.config/nvim/after/plugin/keymap/telescope.lua
@@ -1,27 +1,27 @@
 vim.keymap.set("n", "<C-p>", function()
-    require('telescope.builtin').git_files()
+    require("telescope.builtin").git_files()
 end)
 
 vim.keymap.set("n", "<leader>ps", function()
-    require('telescope.builtin').live_grep()
+    require("telescope.builtin").live_grep()
 end)
 
 vim.keymap.set("n", "<leader>pf", function()
-    require('telescope.builtin').find_files()
+    require("telescope.builtin").find_files()
 end)
 
 vim.keymap.set("n", "<leader>pb", function()
-    require('telescope.builtin').buffers()
+    require("telescope.builtin").buffers()
 end)
 
 vim.keymap.set("n", "<leader>vh", function()
-    require('telescope.builtin').help_tags()
+    require("telescope.builtin").help_tags()
 end)
 
 vim.keymap.set("n", "<leader>gc", function()
-    require('telescope.builtin').git_branches()
+    require("telescope.builtin").git_branches()
 end)
 
 vim.keymap.set("n", "<leader>vrc", function()
-    require('pn.telescope').search_dotfiles()
+    require("pn.telescope").search_dotfiles()
 end)
