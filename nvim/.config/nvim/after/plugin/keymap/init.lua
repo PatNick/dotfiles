a63655a7ac5d820ef712ae40f99cbad6712af2ed
Patrick Nickle <patrick.nickle@gmail.com>
2022-12-15 17:33:32 -0500

    This is a sweet one

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -19,3 +19,5 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>return nil, err<CR>}<CR><esc>kkI<esc>")
 
 vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
