03d91089a3a62f786c705e7c1b805ab93d0091cd
Patrick Nickle <patrick.nickle@gmail.com>
2022-12-15 18:09:41 -0500

    This was so annoying... Disable binary previews

--- nvim/.config/nvim/lua/pn/telescope.lua
+++ nvim/.config/nvim/lua/pn/telescope.lua
@@ -8,6 +8,25 @@ table.insert(vimgrep_arguments, "--hidden")
 table.insert(vimgrep_arguments, "--glob")
 table.insert(vimgrep_arguments, "!**/.git/*")
 
+local Job = require("plenary.job")
+local new_maker = function(filepath, bufnr, opts)
+    filepath = vim.fn.expand(filepath)
+    Job:new({
+        command = "file",
+        args = { "--mime-type", "-b", filepath },
+        on_exit = function(j)
+            local mime_type = vim.split(j:result()[1],"/")[1]
+            if mime_type == "text" then
+                previewers.buffer_previewer_maker(filepath, bufnr, opts)
+            else
+                vim.schedule(function ()
+                    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+                end)
+            end
+        end
+    }):sync()
+end
+
 require("telescope").setup{
 
     defaults = {
@@ -20,6 +39,8 @@ require("telescope").setup{
         file_previewer = previewers.vim_buffer_cat.new,
         grep_previewer = previewers.vim_buffer_vimgrep.new,
         qflist_previewer = previewers.vim_buffer_qflist.new,
+
+        buffer_previewer_maker = new_maker,
 
         mappings = {
             i = {
