36617f9abf98a1855654a78cbe913e876133a56d
Patrick Nickle <patrick.nickle@gmail.com>
2022-12-15 17:44:52 -0500

    I like this for some reason

--- nvim/.config/nvim/lua/pn/set.lua
+++ nvim/.config/nvim/lua/pn/set.lua
@@ -1,38 +1,38 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+local opts = {
+    nu = true,
+    relativenumber = true,
+    errorbells = false,
+    mouse = "",
+    splitbelow = true,
+    splitright = true,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    softtabstop = 4,
+    wrap = false,
+    incsearch = true,
+    backup = false,
+    undodir = os.getenv("HOME") .. "/.vim/undodir",
+    undofile = true,
+    termguicolors = true,
+    scrolloff = 8,
+    signcolumn = "yes",
+    cmdheight = 1,
+    updatetime = 50,
+    colorcolumn = "80"
+}
 
-vim.opt.errorbells = false
+for k, v in pairs(opts) do
+    vim.opt[k] = v
+end
 
-vim.opt.mouse = ""
+local globs = {
+    mapleader = " ",
+    netrw_browse_split = 0,
+    netrw_banner = 0,
+    netrw_winsize = 25,
+}
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-
-vim.opt.smartindent = true
-vim.opt.wrap = false
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
-vim.opt.cmdheight = 1
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
-vim.g.mapleader = " "
+for k, v in pairs(globs) do
+    vim.g[k] = v
+end
