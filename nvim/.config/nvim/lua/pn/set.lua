7cb3af7a8a8c0ca537694491ebd73ccaa01b363e
Patrick Nickle <patrick.nickle@gmail.com>
2023-01-26 16:05:52 -0500

    Fixing warnings

--- nvim/.config/nvim/lua/pn/set.lua
+++ nvim/.config/nvim/lua/pn/set.lua
@@ -33,8 +33,13 @@ local globs = {
     netrw_browse_split = 0,
     netrw_banner = 0,
     netrw_winsize = 25,
+    loaded_python3_provider = 0,
+    loaded_ruby_provider = 0,
+    loaded_node_provider = 0,
+    loaded_perl_provider = 0,
 }
 
 for k, v in pairs(globs) do
     vim.g[k] = v
 end
+
