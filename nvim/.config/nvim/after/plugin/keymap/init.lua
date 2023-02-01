79340901bf74eb7fdedb4fa4d5ebfc1ed03937fc
Patrick Nickle <patrick.nickle@gmail.com>
2023-02-01 10:04:32 -0500

    Feat: Recenter after n and N

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -12,6 +12,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
 vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
