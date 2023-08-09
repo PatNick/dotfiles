a4ce7ab0bb73039a5a648d232301425291959d2e
Patrick Nickle <patrick.nickle@gmail.com>
2023-08-09 15:04:44 -0400

    Messing with treesitter text objects.

--- nvim/.config/nvim/lua/pn/packer.lua
+++ nvim/.config/nvim/lua/pn/packer.lua
@@ -67,6 +67,13 @@ return require("packer").startup(function(use)
         "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate"
     }
+
+    use({
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        after = "nvim-treesitter",
+        requires = "nvim-treesitter/nvim-treesitter",
+    })
+
     use "nvim-treesitter/nvim-treesitter-context"
 
     use "nvim-treesitter/playground"
