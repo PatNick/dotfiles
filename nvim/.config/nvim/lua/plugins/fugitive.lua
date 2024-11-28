a7db10f4bf363459ab632bf0476cd5e12ea1b1ad
Patrick Nickle <patrick.nickle@gmail.com>
2024-11-28 08:35:57 -0500

    feat(git): add bindings for ours vs theirs

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,8 @@ return {
         "tpope/vim-fugitive",
         keys = {
             { "<leader>gs", vim.cmd.G, desc = "GitStatus" },
+            { "gf", "<cmd>diffget //2<CR>" },
+            { "gj", "<cmd>diffget //3<CR>" },
 
         },
     },
