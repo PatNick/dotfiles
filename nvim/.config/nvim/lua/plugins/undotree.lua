562a90fb455a9210d7e3dfb1e2b858e6cc0dae20
Patrick Nickle <patrick.nickle@gmail.com>
2025-01-10 10:11:06 -0500

    feat: focus on toggle

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -3,4 +3,7 @@ return {
     keys = {
         { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle undotree" },
     },
+    init = function(_)
+        vim.g.undotree_SetFocusWhenToggle = 1
+    end,
 }
