a3ea56feb765bf033dfff0661bb9c1fd197b2956
Patrick Nickle <patrick.nickle@gmail.com>
2025-02-24 11:05:11 -0800

    feat: remove branch from status

--- nvim/.config/nvim/lua/pn/status.lua
+++ nvim/.config/nvim/lua/pn/status.lua
@@ -1,16 +1,6 @@
 local M = { }
 
 M.line = function()
-    local function git_branch()
-        local branch = vim.fn.system("git rev-parse --abbrev-ref @ 2>/dev/null | tr -d '\n'")
-        if string.len(branch) > 0 then
-            return branch
-        else
-            return ""
-        end
-    end
-
-    local branch = git_branch()
     local file_name = " %f"
     local modified = " %m"
     local align_right = "%="
@@ -19,7 +9,7 @@ M.line = function()
     local cursor = " %l:%c"
     local percentage = " %P"
 
-    return string.format("%s %s%s%s%s%s%s%s", branch, file_name, modified, align_right, file_encoding, file_format, cursor, percentage)
+    return string.format("%s%s%s%s%s%s%s", file_name, modified, align_right, file_encoding, file_format, cursor, percentage)
 end
 
 return M
