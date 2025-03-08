b865a1da899486d362659efce280ce50e05f80b7
Patrick Nickle <patrick.nickle@gmail.com>
2025-03-08 10:24:18 -0800

    remove old config

--- nvim_old/.config/nvim/after/plugin/keymap/fugitive.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.G)
-vim.keymap.set("n", "<leader>gj", ":diffget //3<CR>")
-vim.keymap.set("n", "<leader>gf", ":diffget //2<CR>")
--- nvim_old/.config/nvim/after/plugin/luasnip.lua
+++
@@ -1,7 +0,0 @@
-local luasnip = require("luasnip")
-local types = require("luasnip.util.types")
-
-luasnip.config.set_config {
-    history = true,
-    updateevents = "Textchanged,TextChangedI",
-}
