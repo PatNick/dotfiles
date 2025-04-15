4cbf6d8726221a9ee749d56f821dca7ae8bd195f
Patrick Nickle <patrick.nickle@gmail.com>
2025-04-15 15:04:00 -0700

    feat: I don't use this

--- awesome/dot-config/awesome/main/theme.lua
+++
@@ -1,7 +0,0 @@
-local gears = require("gears")
-local awful = require("awful")
-local beautiful = require("beautiful")
-local home = os.getenv("HOME")
-
--- Themes define colours, icons, font and wallpapers.
-beautiful.init(home .. "/.config/awesome/themes/clone/theme.lua")
